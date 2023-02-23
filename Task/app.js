188c117b101ae3c4fbf8120ba0d833ad8f355d4c
Muhammad Naveed <121456236+MuhammadNaveed1@users.noreply.github.com>
2023-02-23 19:35:00 +0500

    Add files via upload

---
+++ Task/app.js
@@ -0,0 +1,15 @@
+// Array (Palindrome task)
+var palindrome = 
+
+// // For loop (Palindrome task);
+// var palindrome = prompt("Enter a word");
+// var reverse = "";
+// for (var i = palindrome.length - 1; 0 <= i; i--) {
+//     reverse = reverse + palindrome[i]
+// }
+// if (reverse === palindrome) {
+//     alert("It's a palindrom word");
+// }
+// else {
+//     alert("Not a palindrome word");
+// }
